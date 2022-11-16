536bb8fc191f11dd3b4970ca8613f4d0e7948bd2
Louis Tsang <kawingt2@gmail.com>
2022-11-17 07:22:35 +0800

    set up template engine

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("No!");
+  const ans = "No";
+  res.render("pages/index", {
+    ans,
+  });
 });
 
 app.listen(port, () => {
