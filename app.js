fba6ef33d5d9c3889fc0bc2708643190ff08f78c
Louis Tsang <kawingt2@gmail.com>
2022-11-19 11:30:46 +0800

    improved logging

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require("express");
 require("dotenv").config();
-
+const logger = require("./logger");
 const PORT = process.env.PORT;
 const app = express();
-const pino = require("pino-http")();
+const pinoHTTP = require("pino-http");
 
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/static"));
-app.use(pino);
+app.use(pinoHTTP());
 
 app.get("/", (req, res) => {
   const ans = "No";
+  logger.info(`Rendering Index Page | Ans: ${ans}`);
   res.status(200).render("pages/index", {
     ans,
   });
 });
 
 app.listen(PORT, () => {
+  logger.info("Server started");
   console.log(`Express app running on port ${PORT}`);
 });
